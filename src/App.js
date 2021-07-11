47e61b0b69c71f2fe219d7839d57596fdd198115
Pranjal-K <pranjalcse30@gmail.com>
2021-07-11 21:45:10 +0530

    Project Setup, Added main page to display hotels

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import CardColumns from 'react-bootstrap/CardColumns'
+import { Accordion, Button } from 'react-bootstrap'
+import axios from 'axios';
+import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ({ projects }) => {
 
-export default App;
+  const [data, setData] = React.useState({})
+
+  React.useEffect(() => {
+    axios({
+        method: "get",
+        url: "http://localhost:4000/letsVisitTG/getHotel",
+        params: {
+          location: "Athens",
+          checkin: "2021-07-12",
+          checkout: "2021-07-21",
+          rooms: "1",
+          adults: "2",
+          children: "1" ,
+		  infants: "10"
+        }
+    }).then((resp) => {
+        setData(resp.data)
+    });
+  }, [])
+
+    return (
+          <Accordion>
+			{data.data && data.data.hotels.map((hotel, index)=> {
+				return (
+					<Card key={index}>
+						<Card.Header>
+							<Accordion.Toggle as={Button} variant="link" eventKey={hotel.code}>
+								<p>{hotel.name}</p>
+								<img src={hotel.photo} alt="GeeksforGeeks logo"/>
+							</Accordion.Toggle>
+							<p>Starting Price: {hotel.minprice}</p>
+						</Card.Header>
+							<Accordion.Collapse eventKey={hotel.code}>
+							<Card.Body>
+								{hotel.rates.map((rate)=> {
+									return (
+										<div>
+											<h6 style={{color: "violet"}} key={rate.id}>
+												{rate.room}  Price: {rate.price}
+											</h6>
+											<img src={rate.photo}
+													alt="GeeksforGeeks logo"/>
+											<p>
+												{rate.rate_desc}
+											</p>
+										</div>
+									)
+								})}
+							</Card.Body>
+						</Accordion.Collapse>
+					</Card>
+				)
+			})}
+        </Accordion>   
+    )
+};
+
+export default App
